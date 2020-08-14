776eb8ffc67352691497a8bcc9129fd74988568b
Mykola-DMT <50734364+Mykola-DMT@users.noreply.github.com>
2020-08-14 17:44:26 +0300

    master

--- app/models.py
+++ app/models.py
@@ -8,8 +8,15 @@ class Item(db.Model):
     size_i=db.Column(db.Integer)
     price=db.Column(db.Integer)
     day=db.Column(db.Date)
+    user_id=db.Column(db.Integer, db.ForeignKey('user.id'))
     #isold=db.Column(db.Boolean)
 
     def __repr__(self):
         return '<Item {} {} size= {} price= {}>'.format(self.typename,self.itemname,self.size_i,self.price)
 
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    username=db.Column(db.String(64),index=True,unique=True)
+    email=db.Column(db.String(120),index=True,unique=True)
+    password_hash=db.Column(db.String(128))
+    items=db.relationship('Item', backref='author', lazy='dynamic')
