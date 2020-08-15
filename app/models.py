3af219485a094c19c2d51f010446c6c4d3a58441
Mykola-DMT <50734364+Mykola-DMT@users.noreply.github.com>
2020-08-15 14:39:01 +0300

    master

--- app/models.py
+++ app/models.py
@@ -24,7 +24,7 @@ class User(UserMixin,db.Model):
     password_hash=db.Column(db.String(128))
     items=db.relationship('Item', backref='author', lazy='dynamic')
 
-    def set_password(self,pasword):
+    def set_password(self,password):
         self.password_hash= generate_password_hash(password)
 
     def check_password(self,password):
