f0d71fbfae206bb513b4f5d30d3244f0c1980db7
Mykola-DMT <50734364+Mykola-DMT@users.noreply.github.com>
2020-08-16 22:46:58 +0300

    master

--- app/routes.py
+++ app/routes.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 from app import app
 from flask import render_template,flash, redirect, request
-from app.forms import AddForm,SearchForm
+from app.forms import AddForm,SearchForm,RegistrationForm,LoginForm
 from datetime import date,datetime
 from app.db_setup import init_db, db_session
 from app.models import Item, User
@@ -26,6 +26,20 @@ def login():
             next_page=url_for('index')
         return redirect(next_page)
     return render_template('login.html',title='Sign In', form=form)
+
+@app.route('/register', methods=['GET','POST'])
+def register():
+    if current_user.is_authenticated:
+        return redirect(url_for('index')
+    form= RegistrationForm()
+    if form.validate_on_submit():
+        user=User(username=form.username.data,email=form.email.data)
+        user.set_password(form.password.data)
+        db.session.add(user)
+        db.session.commit()
+        flash('You registered!')
+        return redirect(url_for('index'))
+    return  render_template('register.html',form)
 
 @app.route('/logout')
 def logout():
