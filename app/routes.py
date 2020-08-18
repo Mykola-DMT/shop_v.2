ae7cfc208697900a392127b513cc76f26a011edd
Mykola-DMT <50734364+Mykola-DMT@users.noreply.github.com>
2020-08-18 22:22:01 +0300

    master

--- app/routes.py
+++ app/routes.py
@@ -17,15 +17,16 @@ def login():
     form = LoginForm()
     if request.method=='POST' and form.validate():
         user = User.query.filter_by(username=form.username.data).first()
-        flash('Succes')
+        
         if user is None or not user.check_password(form.password.data):
             flash('Invalid username or password')
-            return redirect (url_for('login'))
+            return redirect(url_for('index'))
         login_user(user, remember=form.remember_me.data)
         # next_page = request.args.get('next')
         # if not next_page or url_parse(next_page).netloc != '':
         #     next_page=url_for('index')
         return redirect(url_for('index'))
+    
     return render_template('login.html', form=form)
 
 @app.route('/register', methods=['GET','POST'])
