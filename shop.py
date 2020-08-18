c3ff6459507dee52f632de500ed3754d82b885ae
Mykola-DMT <50734364+Mykola-DMT@users.noreply.github.com>
2020-08-18 18:07:53 +0300

    master

--- shop.py
+++ shop.py
@@ -1,6 +1,6 @@
 from app import app
-from app.models import Item
+from app.models import Item,User
 
 @app.shell_context_processor
 def make_shell_context():
-    return {'db':db, 'Item':Item}
+    return {'db':db, 'Item':Item, 'User':User}
